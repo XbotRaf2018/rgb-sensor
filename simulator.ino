80e8c751ad06d481ce40ba872c502224e2e6bd9a
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-17 15:34:18 +0200

    Simulator

--- simulator.ino
+++ simulator.ino
@@ -18,6 +18,22 @@ This is simple sketch that simulates RGB LED and photo transistor.
 #define YELLOW 3
 #define BLUE 4
 
+// Test
+
+#define TRESHOLD 100
+
+// Callibration
+
+int callibration[][3] = {
+	{0, 0, 0}, // ORANGE
+	{0, 0, 0}, // BLACK
+	{0, 0, 0}, // GREEN
+	{0, 0, 0}, // YELLOW
+	{0, 0, 0}, // BLUE
+};
+
+// Setup
+
 void setup(){
 	pinMode(RED_LED_IN, INPUT);
 	pinMode(GREEN_LED_IN, INPUT);
@@ -30,6 +46,21 @@ void setup(){
 	randomSeed(analogRead(0));
 }
 
+int color = 0;
+
+// Loop
+
 void loop(){
+	if (Serial.available() > 0) {
+		color = Serial.parseInt();
+	}
+
+	float output = 0;
+	
+	int red = digitalRead(RED_LED_IN);
+	int green = digitalRead(GREEN_LED_IN);
+	int blue = digitalRead(BLUE_LED_IN);
+
+	
 }
 
